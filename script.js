f96fc5f8a22346722bd6f5af827059842fd640d3
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2021-11-10 17:40:03 -0600

    new branch for random colors on squares

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Your JS goes here
+checkerBoard()
+
+function checkerBoard() {
+    let body = document.getElementsByTagName('body')[0];
+    
+    for(let i = 0; i < 63; i++) {
+    let div = document.createElement('div');
+
+    div.style.width = '11.1%';
+    div.style.float = 'left';
+    div.style.paddingBottom = '11.1%'    
+
+    if (i % 2 === 0) {
+        div.style.backgroundColor = 'red'
+    } else {
+        div.style.backgroundColor = 'black'
+    }
+    body.appendChild(div)
+    }
+}
