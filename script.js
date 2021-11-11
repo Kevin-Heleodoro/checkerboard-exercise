e800a3d83f8a46786dd269033dd78cc89ccfc269
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2021-11-11 14:50:11 -0600

    flashing complete. have not yet compared to solution

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
+// Your JS goes here
 checkerBoard()
 
 function checkerBoard() {
     let body = document.getElementsByTagName('body')[0];
-    
+    // body.style.background = 'linear-gradient(to right, rgba(21, 21, 213, 0), rgba(21, 21, 213, 1))'; FAILED 
+    // body.style.background = 'linear-gradient(135deg, blue, yellow)';
     for(let i = 0; i < 63; i++) {
     let div = document.createElement('div');
-
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
     div.style.width = '11.1%';
     div.style.float = 'left';
     div.style.paddingBottom = '11.1%'    
+    div.style.backgroundColor = "#" + randomColor;
+    div.classList.add('square')
 
-    if (i % 2 === 0) {
-        div.style.backgroundColor = 'red'
-    } else {
-        div.style.backgroundColor = 'black'
-    }
+    // if(i % 2 === 0){
+    //     // div.style.backgroundColor = `rgba(14, 14, 240, ${.02 * i})`
+    //     // div.style.backgroundColor = 'blue' FAILED
+    // } else {
+    //     // div.style.backgroundColor = `rgba(249, 249, 41, ${.02 * i})`
+    //     // div.style.backgroundColor = 'yellow' FAILED
+    // }
     body.appendChild(div)
     }
+    
 }
+
+
+setInterval(function flashingColors() {
+    let div = document.querySelectorAll('div');
+    let body = document.getElementsByTagName('body');
+    
+
+    div.forEach((elem) => {
+        let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        elem.style.backgroundColor = "#" + randomColor;
+    })
+    // body.appendChild(div);
+}, 2000);
 
 // function checkerBoard() {
 //     let body = document.getElementsByTagName('body')
